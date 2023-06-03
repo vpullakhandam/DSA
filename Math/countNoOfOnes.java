e14fa5a99a04981f41ead4d93cbce99c8b2300f7
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2023-06-03 16:13:13 +0530

    added code
    code for count of number of ones

---
+++ Math/countNoOfOnes.java
@@ -0,0 +1,24 @@
+package Math;
+import java.util.*;
+public class countNoOfOnes {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int sum=0;
+        for(int i=0;i<=n;i++){
+            sum=sum+count(i);
+        }
+        System.out.println(sum);
+    }
+    public static int count(int i) {
+        int no=0;
+        while(i>0){
+            int rem=i%10;
+            if(rem==1){
+                no=no+1;
+            }
+            i=i/10;
+        }
+        return no;
+    }
+}
