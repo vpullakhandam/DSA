ab78114e2521c71843acfe566de4b5354f7da0ff
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2023-06-03 20:13:48 +0530

    added new file
    code for palindrome number

---
+++ Math/palindrome_number.java
@@ -0,0 +1,31 @@
+package Math;
+import java.util.*;
+// https://leetcode.com/problems/palindrome-number/
+public class palindrome_number {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n =in.nextInt();
+        System.out.println(isPalindrome(n));
+    }
+    public static boolean isPalindrome(int x) {
+        // if(x==0){
+        //     return true;
+        // }
+        int y=x;
+        int s=0;
+        if(x>=0){
+            while(x!=0){
+                s=s*10+(x%10);
+                x=x/10;
+            }
+        } else {
+            return false;
+        }
+
+        if(s==y){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
