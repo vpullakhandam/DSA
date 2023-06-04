d2f507742eafabab3da13933bcdcdba1e89433db
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2023-06-04 12:55:12 +0530

    added new file
    code for removing duplicates in an array

---
+++ array/removeDuplicatesFromSortedArray.java
@@ -0,0 +1,24 @@
+package array;
+import java.util.*;
+public class removeDuplicatesFromSortedArray {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        System.out.println(removeDuplicates(arr));
+    }
+
+    public static int removeDuplicates(int[] arr) {
+        int i=0;
+        for(int j=1;j<arr.length;j++){
+            if(arr[i]!=arr[j]){
+                i++;
+                arr[i]=arr[j];
+            }
+        }
+        return i+1;
+    }
+}
