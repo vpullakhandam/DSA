e0884d8737b17aa9f8e15cfee435e9ac19279624
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2023-06-04 18:25:26 +0530

    added file
    code for fib number using recursion

---
+++ Math/fib_usingRecursion.java
@@ -0,0 +1,15 @@
+package Math;
+import java.util.*;
+public class fib_usingRecursion {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(fib(n));
+    }
+    public static int fib(int n) {
+        if(n<=1){
+            return n;
+        }
+        return fib(n-1)+fib(n-2);
+    }
+}
