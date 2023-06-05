df456ab4d2eac3e5f3d74280882dd8bd179e475a
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2023-06-05 14:52:16 +0530

    added new code
    code for good pairs

---
+++ array/goodPairs.java
@@ -0,0 +1,24 @@
+package array;
+import java.util.*;
+public class goodPairs {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=in.nextInt();
+        }
+        System.out.println(numIdenticalPairs(arr));
+    }
+    public static int numIdenticalPairs(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
